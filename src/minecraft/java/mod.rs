f757ea9b2550089bf020640c40e55a8617fbdb31
Batuhan Aksoy <batuhaniletisimonline@gmail.com>
2024-06-15 19:01:08 +0300

    feat: Add Java Delta (21)

--- src/minecraft/java/mod.rs
+++ src/minecraft/java/mod.rs
@@ -1,4 +1,9 @@
-use std::{env, fmt::Display, path::{Path, PathBuf}, process::Command};
+use std::{
+    env,
+    fmt::Display,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 use ::serde::Deserialize;
 
@@ -8,8 +13,9 @@ use self::serde::{JavaManifest, JavaRuntime};
 
 mod serde;
 
-#[derive(Debug, PartialEq, PartialOrd,Deserialize)]
+#[derive(Debug, PartialEq, PartialOrd, Deserialize)]
 pub enum JavaVersion {
+    Delta,
     Gamma,
     Alpha,
     Beta,
@@ -18,27 +24,29 @@ pub enum JavaVersion {
 
 const JAVA_RUNTIMES : &str = "https://launchermeta.mojang.com/v1/products/java-runtime/2ec0cc96c44e5a76b9c8b7c39df7210883d12871/all.json";
 
-impl Display for JavaVersion{
+impl Display for JavaVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self{
-            Self::Alpha=>{
-                write!(f,"java-runtime-alpha")
-            },
-            Self::Beta=>{
-                write!(f,"java-runtime-beta")
-            },
-            Self::Gamma=>{
-                write!(f,"java-runtime-gamma")
-            },
-            Self::Legacy=>{
-                write!(f,"jre-legacy")
+        match self {
+            Self::Delta => {
+                write!(f, "java-runtime-delta")
+            }
+            Self::Alpha => {
+                write!(f, "java-runtime-alpha")
+            }
+            Self::Beta => {
+                write!(f, "java-runtime-beta")
+            }
+            Self::Gamma => {
+                write!(f, "java-runtime-gamma")
+            }
+            Self::Legacy => {
+                write!(f, "jre-legacy")
             }
         }
     }
 }
 
-
-pub fn get_java_path() -> Option<PathBuf>{
+pub fn get_java_path() -> Option<PathBuf> {
     match Command::new("java").arg("-version").output() {
         Ok(output) => {
             let output = std::str::from_utf8(&output.stderr).unwrap_or("");
@@ -46,18 +54,15 @@ pub fn get_java_path() -> Option<PathBuf>{
                 let path = output.split(' ').collect::<Vec<&str>>()[3]
                     .split('=')
                     .collect::<Vec<&str>>()[1];
-                
+
                 #[cfg(target_os = "windows")]
-                return Some(PathBuf::from(path).join("bin").join("java.exe"))
+                return Some(PathBuf::from(path).join("bin").join("java.exe"));
 
                 // Todo : Other operating systems...
-                
             }
             None
         }
-        Err(e) => {
-            None
-        }
+        Err(e) => None,
     }
 }
 pub fn detect_java_by_cmd() -> Option<JavaVersion> {
@@ -70,13 +75,10 @@ pub fn detect_java_by_cmd() -> Option<JavaVersion> {
                     .split('.')
                     .collect::<Vec<&str>>();
 
-                let java_version = if split[0] != "1"{
-                    split[0]
-                }else{
-                    split[1]
-                };
+                let java_version = if split[0] != "1" { split[0] } else { split[1] };
 
                 match java_version {
+                    "21" => return Some(JavaVersion::Delta),
                     "17" => return Some(JavaVersion::Gamma),
                     "16" => return Some(JavaVersion::Alpha),
                     "8" => return Some(JavaVersion::Legacy),
@@ -87,9 +89,7 @@ pub fn detect_java_by_cmd() -> Option<JavaVersion> {
             }
             None
         }
-        Err(e) => {
-            None
-        }
+        Err(e) => None,
     }
 }
 
@@ -120,6 +120,7 @@ pub async fn get_manifest_by_version(version: &JavaVersion) -> Result<JavaManife
     };
 
     let url = &java_runtime[format!("{}-{}", os, arch).as_str()][match version {
+        JavaVersion::Delta => "java-runtime-delta",
         JavaVersion::Gamma => "java-runtime-gamma",
         JavaVersion::Alpha => "java-runtime-alpha",
         JavaVersion::Beta => "java-runtime-beta",
@@ -130,5 +131,3 @@ pub async fn get_manifest_by_version(version: &JavaVersion) -> Result<JavaManife
 
     get_json::<JavaManifest>(url).await
 }
-
-
