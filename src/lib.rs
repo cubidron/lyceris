3fa6cc7541453f7b7246decb3c1f4c3e52d0096c
Batuhan Aksoy <61664394+BatuhanAksoyy@users.noreply.github.com>
2024-11-01 16:28:27 +0300

    fix: Tests

--- src/lib.rs
+++ src/lib.rs
@@ -39,9 +39,6 @@ mod tests {
         minecraft::{version::MinecraftVersion, Instance},
         reporter::Reporter,
     };
-    use axum::extract::Query;
-    use axum::Router;
-    use axum::routing::get;
     use oauth2::basic::BasicClient;
     use oauth2::reqwest::async_http_client;
     use oauth2::{AuthType, AuthUrl, AuthorizationCode, ClientId, CsrfToken, DeviceAuthorizationUrl, PkceCodeChallenge, RedirectUrl, Scope, StandardDeviceAuthorizationResponse, TokenResponse, TokenUrl};
