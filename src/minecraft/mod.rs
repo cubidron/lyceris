63b396b473e4fcd77c37ef9873c02fa495990200
Batuhan Aksoy <61664394+BatuhanAksoyy@users.noreply.github.com>
2024-12-16 20:31:44 +0300

    :fire: refactor: Remove unnecessary
    - Remove `java` module since we don't need it.
    - Remove unused architecture initializations.

--- src/minecraft/mod.rs
+++ src/minecraft/mod.rs
@@ -18,22 +18,8 @@ pub const TARGET_ARCH: &str = "x86";
 #[cfg(target_arch = "x86_64")]
 pub const TARGET_ARCH: &str = "x86_64";
 
-#[cfg(target_arch = "arm")]
-pub const NATIVE_ARCH: &str = "arm";
-
 #[cfg(target_arch = "aarch64")]
 pub const TARGET_ARCH: &str = "aarch64";
-
-#[cfg(target_arch = "x86")]
-pub const NATIVE_ARCH: &str = "32";
-#[cfg(target_arch = "x86_64")]
-pub const NATIVE_ARCH: &str = "64";
-
-#[cfg(target_arch = "arm")]
-pub const NATIVE_ARCH: &str = "64";
-
-#[cfg(target_arch = "aarch64")]
-pub const NATIVE_ARCH: &str = "64";
 
 #[cfg(target_os = "windows")]
 pub const CLASSPATH_SEPARATOR: &str = ";";
